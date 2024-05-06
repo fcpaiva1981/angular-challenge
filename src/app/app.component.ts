225fdb40e19d0a31b90cb9ed3b1b927dc9ce7c93
Fernando Campos de Paiva <fernandocampospaiva@hotmail.com>
2024-05-06 12:04:32 -0300

    Ações no formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgForm} from '@angular/forms';
+import {Usuario} from "./model/usuario";
+import {UsuariosService} from "./services/usuarios.service";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,40 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angularchallenge';
+  usuario = {} as Usuario;
+  usuarios: Usuario[] = [];
+
+  constructor(private usuarioService: UsuariosService) {}
+
+  ngOnInit() {
+    this.getUsuarios();
+  }
+
+  getUsuarios() {
+    this.usuarioService.getUsuarios().subscribe((usuarios: Usuario[]) => {this.usuarios= usuarios});
+  }
+
+  editUsuario(usuario : Usuario) {
+    this.usuario = { ...usuario };
+  }
+
+  deleteUsuario(usuarios: Usuario) {
+    this.usuarioService.deleteUsuario(usuarios).subscribe(() =>{this.getUsuarios()});
+  }
+
+  saveUsuario(form: NgForm){
+    if(this.usuario.id !== undefined){
+      this.usuarioService.updateUsuario(this.usuario).subscribe(() =>{this.cleanForm(form)});
+    }else{
+      this.usuarioService.saveUsuario(this.usuario).subscribe(() =>{this.cleanForm(form)});
+    }
+  }
+
+  cleanForm(form: NgForm) {
+    this.getUsuarios();
+    form.resetForm();
+    let usuario = {} as Usuario;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,6 @@
 import { Routes } from '@angular/router';
-import {UsuariosListComponent} from "./componets/usuarios-list/usuarios-list.component";
+
 
 export const routes: Routes = [
-  {
-    path:"",
-    component:UsuariosListComponent,
-  },
-  {
-    path:"usuarios-list",
-    component:UsuariosListComponent,
-  }
+
 ];
